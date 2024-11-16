7a3f6ded99ee708f58ea8791add73e30a448c396
mackdog <mmccorma95@gmail.com>
2024-11-16 12:02:11 -0600

    added in a new game button and a pop up modal for the cricket winner

--- src/main/Components/cricket/Cricket.jsx
+++ src/main/Components/cricket/Cricket.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import ScoringLine from "./ScoringLine";
-import {useSelector} from "react-redux";
-import {getPlayers, getRounds} from "../../selectors/cricketSelector";
+import {useDispatch, useSelector} from "react-redux";
+import {getPlayers, getRounds, getWinningPlayer} from "../../selectors/cricketSelector";
+import WinningModal from "../WinningModal";
+import {resetRounds} from "../../actions/cricketActions";
 
 const Cricket = () => {
+    const dispatch = useDispatch();
     const rounds = useSelector(getRounds);
     const players = useSelector(getPlayers);
+    const winningPlayerAvailable = useSelector(getWinningPlayer);
+
+    const newGameClick = () => {
+        dispatch(resetRounds());
+    };
+
     return (
         <div>
+            {winningPlayerAvailable && <WinningModal name={winningPlayerAvailable.name}/>}
+            <div className="newGameButtonGrid">
+                <button className="newGameButton" onClick={newGameClick}>
+                    New Game
+                </button>
+            </div>
             <div className="cricket-player-section">
                 {players.map(player => <span className="cricket-player">{player.name}: {player.totalScore}</span>)}
             </div>
--- src/main/selectors/cricketSelector.js
+++ src/main/selectors/cricketSelector.js
@@ -21,3 +21,19 @@ export const getPlayers = createSelector(
     cricketState,
     players => players.players,
 );
+
+export const getWinningPlayer = createSelector(
+    cricketState,
+    players => {
+        const player1 = players.players.find(player => player.name === 'Player 1');
+        const player2 = players.players.find(player => player.name === 'Player 2');
+
+        if (player1.tally.every(value => value[1] === 3) && player2.tally.every(value => value[1] === 3)) {
+            if (player1.totalScore > player2.totalScore) {
+                return player1;
+            }
+            return player2;
+        }
+        return false;
+    }
+);
